d37bd8704368eec61e3566a13f85661053e1cb79
raymondhoang <raymond.hoang1@hotmail.com>
2024-01-08 00:08:27 -0500

    Update information

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, redirect, request, url_for, flash
-import os
-from forms import CreatePostForm, RegisterForm, LoginForm
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_ckeditor import CKEditor
-from datetime import datetime
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some random key'
 
-db = SQLAlchemy(app)
 # allows app to use bootstrap
 bootstrap = Bootstrap(app)
-# allows app to use ckeditor in html
-ckeditor = CKEditor(app)
 
 
 @app.route("/")
